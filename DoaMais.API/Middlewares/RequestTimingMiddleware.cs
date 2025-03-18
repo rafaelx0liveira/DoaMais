7e3b2f5926dd6dd2d69863f7e254e05b752cb678
rafaelx0liveira <rafaelaparecido.oliveirasilva@gmail.com>
2025-03-17 23:00:34 -0300

    feat: middleware for request timing

---
+++ DoaMais.API/Middlewares/RequestTimingMiddleware.cs
@@ -0,0 +1,34 @@
+﻿using Serilog.Context;
+using System.Diagnostics;
+
+namespace DoaMais.API.Middlewares
+{
+    public class RequestTimingMiddleware
+    {
+        private readonly RequestDelegate _next;
+        private readonly ILogger<RequestTimingMiddleware> _logger;
+
+        public RequestTimingMiddleware(RequestDelegate next, ILogger<RequestTimingMiddleware> logger)
+        {
+            _next = next;
+            _logger = logger;
+        }
+
+        public async Task Invoke(HttpContext context)
+        {
+            var stopwatch = Stopwatch.StartNew();
+            await _next(context);
+            stopwatch.Stop();
+
+            var elapsedMs = stopwatch.ElapsedMilliseconds;
+
+            using (LogContext.PushProperty("http.response_time", elapsedMs))
+            using (LogContext.PushProperty("http.request_method", context.Request.Method))
+            using (LogContext.PushProperty("http.request_path", context.Request.Path))
+            using (LogContext.PushProperty("http.response_status", context.Response.StatusCode))
+            {
+                _logger.LogInformation($"HTTP {context.Request.Method} {context.Request.Path} responded {context.Response.StatusCode} in {elapsedMs}ms");
+            }
+        }
+    }
+}
