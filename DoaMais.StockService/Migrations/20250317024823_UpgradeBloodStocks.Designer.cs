d42744c10bbf6de2e938748f27e0915ad9e80d53
rafaelx0liveira <rafaelaparecido.oliveirasilva@gmail.com>
2025-03-17 23:01:20 -0300

    build: upgrading blood stock

--- DoaMais.StockService/Migrations/20250317024823_UpgradeBloodStocks.Designer.cs
+++ DoaMais.StockService/Migrations/20250317024823_UpgradeBloodStocks.Designer.cs
@@ -4,6 +4,7 @@ using DoaMais.StockService.Model.Context;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace DoaMais.StockService.Migrations
 {
     [DbContext(typeof(SQLServerContext))]
-    partial class SQLServerContextModelSnapshot : ModelSnapshot
+    [Migration("20250317024823_UpgradeBloodStocks")]
+    partial class UpgradeBloodStocks
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ DoaMais.StockService/Migrations/20250317024823_UpgradeBloodStocks.cs
@@ -0,0 +1,60 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace DoaMais.StockService.Migrations
+{
+    /// <inheritdoc />
+    public partial class UpgradeBloodStocks : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "RHFactor",
+                table: "BloodStocks",
+                type: "nvarchar(450)",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "BloodType",
+                table: "BloodStocks",
+                type: "nvarchar(450)",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_BloodStocks_BloodType_RHFactor",
+                table: "BloodStocks",
+                columns: new[] { "BloodType", "RHFactor" },
+                unique: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "IX_BloodStocks_BloodType_RHFactor",
+                table: "BloodStocks");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "RHFactor",
+                table: "BloodStocks",
+                type: "nvarchar(max)",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(450)");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "BloodType",
+                table: "BloodStocks",
+                type: "nvarchar(max)",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(450)");
+        }
+    }
+}
