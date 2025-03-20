b1a45670719a7070fa99c5a9c9c56c603451a83e
rafaelx0liveira <rafaelaparecido.oliveirasilva@gmail.com>
2025-03-19 23:52:56 -0300

    test: class for mocked objects

---
+++ DoaMais.Tests/Utils/TestsUtils.cs
@@ -0,0 +1,52 @@
+﻿using FizzWare.NBuilder;
+
+namespace DoaMais.Tests.Utils
+{
+    public static class TestsUtils
+    {
+        public static T CreateMockedObject<T>() where T : class
+        {
+            return Builder<T>.CreateNew().Build();
+        }
+
+        public static IList<T> CreateMockedList<T>(int count) where T : class
+        {
+            return Builder<T>.CreateListOfSize(count).Build();
+        }
+
+        public static Guid CreateMockedGuid()
+        {
+            return Builder<Guid>.CreateNew().Build();
+        }
+
+        public static string CreateMockedString()
+        {
+            return "MockedString";
+        }
+
+        public static string CreateMockedEmail()
+        {
+            return "email@example.com";
+        }
+
+        public static DateTime CreateMockedDateTime()
+        {
+            return Builder<DateTime>.CreateNew().Build();
+        }
+
+        public static T CreateMockedEnum<T>() where T : struct
+        {
+            return Builder<T>.CreateNew().Build();
+        }
+
+        public static string CreateMockedStringWithLenght(int lenght)
+        {
+            return new string('a', lenght);
+        }
+
+        public static int CreateMockedInt()
+        {
+            return 1;
+        }
+    }
+}
